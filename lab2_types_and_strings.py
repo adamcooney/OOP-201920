d04343fe9e383fe0d49aa8eae19bdb055ba4d4fb
adamcooney <56026401+adamcooney@users.noreply.github.com>
2019-10-29 15:21:19 +0000

    hello world

--- lab2_types_and_strings.py
+++ lab2_types_and_strings.py
@@ -14,23 +14,37 @@ class Types_and_Strings:
         print("Originally entered: "+ message)
 
         # print only first and last of the sentence
+        print("\nFirst Letter:" + message[0])
+        print("Last Letter: " + message[-1])
 
 
         # use slice notation
-
+        print("\nusing slice notation")
+        print("First 3 letters: " + message[0:3])
+        print("Last 3 letters: " + message[-3:])
+        print("Up to 4th index: " + message[:4])
+        print("Second index onwards: " + message[2:])
+        print("Displaying whole noun: " + message[:])
 
         # escaping a character
+        print("\nHe said \"that\'s fantastic\"!")
 
 
         # find all a's in the input word and count how many there are
-
+        print("First position of a:")
+        print(message.find(str('a')))
+        print("Occurrences of a:")
+        print(message.count(str('a')))
 
         # replace all occurences of the character a with the - sign
         # try this first by assignment of a location in a string and
         # observe what happens, then use replace()
+        print(message.replace('a', '-'))
 
 
         # printing only characters at even index positions
+        for i in range(0, len(message), 2):
+            print(message[i])
 
 
     def play_with_lists(self):
@@ -38,9 +52,14 @@ class Types_and_Strings:
         print("Originally entered: "+ message)
 
         # hand the input string to a list and print it out
-
+        original_sentence = []
+        original_sentence = message.split()
+        print(original_sentence)
 
         # append a new element to the list and print
+        user_input = input("Add word to the sentence: ")
+        original_sentence.append(user_input)
+        print(original_sentence)
 
 
         # remove from the list in 3 ways
@@ -60,5 +79,5 @@ class Types_and_Strings:
 
 
 tas = Types_and_Strings()
-tas.play_with_strings()
-#tas.play_with_lists()
+#tas.play_with_strings()
+tas.play_with_lists()
--- lab3_word_scramble.py
+++ lab3_word_scramble.py
@@ -19,11 +19,49 @@ class WordScramble:
         # particularly good to use is to switch the first two
         # and the last two
         # this only makes sense if you have a world that is longer than 3
+        if len(self.user_input) > 3:
+            new_word = self.user_input[0] + self.user_input[2] + self.user_input[1] + self.user_input[3:]
+        elif len(self.user_input) <= 3:
+            new_word = self.user_input
+        else:
+            print("try again")
+            new_word = False
+
+        print(new_word)
+
 
 
         # now try to scramble one sentence
         # do just words first, then you can move on to work on
         # punctuation
+        sentence = self.user_input.strip().split()  #strip ensures no white space at the end
+        for index, word in enumerate(sentence):
+                if len(word) > 3:
+                    #need container i can swap stuff in
+                    temp_word = list(word)
+                    if(',' in temp_word):
+                        #swap but keep position of comma
+                        temp = temp_word[1]
+                        temp_word[1] = temp_word[-3]
+                        temp_word[-3] = temp
+
+                    else:
+                        #normal swap
+                        temp = temp_word[1]
+                        temp_word[1] = temp_word[2]
+                        temp_word[2] = temp
+
+                        temp_word = ''.join(temp_word)
+                        sentence[index] = temp_word
+
+                else:
+                    #new word is the same as input
+                    sentence[index] = word
+
+                    sentence = ' '.join(sentence)
+                    print(sentence)
+
+                #reassemble the word and then the sentence
 
 word_scrambler = WordScramble()
 word_scrambler.scramble()
