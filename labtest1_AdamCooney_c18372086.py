ae4178ca22636672b3c4da81c3656d82e11c402c
adamcooney <56026401+adamcooney@users.noreply.github.com>
2019-11-12 19:25:29 +0000

    hello world

---
+++ labtest1_AdamCooney_c18372086.py
@@ -0,0 +1,34 @@
+class MuseumMoney:
+
+        myFile = open("MuseumsCollectionsArchives_2015.csv", "r")
+
+        for line in myFile:
+                print(line)
+
+        thisDict = {
+                myFile
+        }
+
+        def get_museum_area(self, amount):
+                from queue import Queue
+                lines = Queue(maxsize=600)
+
+                if line.full():
+                        print("line is full")
+
+                for i in range(1, 600):
+                        line.put(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
